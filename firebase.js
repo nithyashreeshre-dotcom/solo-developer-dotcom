3d747e3c8ad891ac79de0669e2bb4df479812880
SHREEVATSA <nithyashree.shre@gmail.com>
2026-06-23 17:24:27 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,4 @@
-─────────────────
-  import { initializeApp }                        from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { initializeApp }                        from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
   import { getAnalytics, logEvent }               from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
   import {
     getAuth,
